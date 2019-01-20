b54c18323dd9286a458461a454602500ca4508d3
Matteoxx <41827354+Matteoxx@users.noreply.github.com>
2019-01-20 18:09:22 +0100

    dodane wyszukiwanie urzadzen bluetooth

--- screens/Connect.js
+++ screens/Connect.js
@@ -1,32 +1,88 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-
-
+import {StyleSheet, Text, View, AsyncStorage, TouchableOpacity} from 'react-native';
+import { BleManager } from 'react-native-ble-plx';
+import { Navigation } from 'react-native-navigation';
+ 
 export default class Connect extends Component {
-
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.default}>Connect screen!</Text>
-      </View>
-    );
-  }
-
+ 
+    constructor(props) {
+        super(props);
+        this.manager = new BleManager();
+    }
+ 
+    checkBluetoothState() {
+        const subscription = this.manager.onStateChange((state) => {
+            if (state === 'PoweredOn') {
+                this.scanAndConnect();
+                subscription.remove();
+            }
+ 
+        }, true)
+    }
+ 
+    sendAndConnect() {
+        this.manager.startDeviceScan(null, null, (error, device) => {
+            if (error) {
+                console.log(error);
+                return null;
+            }
+ 
+            if(device.name === 'MLT-BT05'){
+                this.manager.stopDeviceScan();
+                return device.connect()
+                    .then((device) => {
+                        return device.discoverAllServicesAndCharacteristics();
+                    }).then((characteristic) =>{
+                        const device = {
+                            id: characteristic.id,
+                            service: 'FFE0',
+                            char: 'FFE1',
+ 
+                        };
+                        return this.manager.writeCharacteristicWithResponseForDevice(device.id, device.service, device.char, btoa('green'))
+                            .then(response => {
+                                console.log(response);
+                        })
+                    }).catch((error) => {
+                        console.log('Error', error);
+                    });
+ 
+ 
+            }
+            console.log(device)
+            // return AsyncStorage.setItem('device', JSON.stringify(device)).then(()=> {
+            //   Navigation.mergeOptions('DEV', {
+            //     bottomTab: {
+            //       badge: '+'
+            //     },
+            //     bottomTabs: {
+            //       currentTabId: 'DEV'
+            //     }
+            //   });
+            // });
+        });
+    }
+ 
+    render() {
+        return (
+            <View style={styles.container}>
+                  <TouchableOpacity onPress={() => this.sendAndConnect()}>
+                      <Text style={styles.default}>Connect screen!</Text>
+                  </TouchableOpacity>
+ 
+            </View>
+        );
+    }
+ 
 }
-
+ 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  default: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  }
+    container: {
+        flex: 1,
+    },
+    default: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10
+    }
 });
